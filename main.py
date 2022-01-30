302968bc470a202fc812a104f60414098fdbaeca
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2022-01-30 12:36:50 -0700

    Added image load and scaled to window size

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 # A GUI app to watermark an image
 
-from tkinter import Tk, Button, filedialog, Label, N
+from tkinter import Tk, Button, filedialog, Label
 from PIL import ImageTk, Image
+
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 500
+panel = None
+WATERMARK = None
 
 
 def open_image():
+    global panel
+    # Check for existing image in window and remove
+    if panel != None:
+        panel.destroy()
+    # Get filename and open image
     filename = openfilename()
     img = Image.open(filename)
-    img = img.resize((500, 500), Image.ANTIALIAS)
-    img = ImageTk.PhotoImage(img)
-    panel = Label(image=img)
-    panel.image = img
-    panel.place(y=25, relwidth=1.0)
+    img_width, img_height = img.size  # Find size of image for scaling
+    img_scale = (WINDOW_WIDTH - 50) / img_width  # Scale image to fit width
+    # Check height of image vs window height and reset scale if needed
+    if img_height * img_scale > WINDOW_HEIGHT - 30:
+        img_scale = (WINDOW_HEIGHT - 30) / img_height
+    preview_img = img.resize((int(img_width*img_scale),
+                              int(img_height*img_scale)),
+                             Image.ANTIALIAS)
+    preview_img = ImageTk.PhotoImage(preview_img)
+    panel = Label(image=preview_img)
+    panel.image = preview_img
+    panel.place(relwidth=1.0)
 
 
 def openfilename():
@@ -27,10 +44,11 @@ def openfilename():
 def main():
     window = Tk()
     window.title("Simple Watermarking App")
-    window.geometry("500x500")
-    window.resizable(width=True, height=True)
+    window.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
+    # window.resizable(width=True, height=True)
     open_button = Button(text="Open Image", command=open_image)
-    open_button.place(width=100, relx=0.5, x=-50)
+    # Place button at bottom
+    open_button.place(width=100, relx=0.5, x=-50, rely=1, y=-25)
     window.mainloop()  # Keep the window open
 
 
