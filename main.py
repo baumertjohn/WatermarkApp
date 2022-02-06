3c58c0b731e0d5b0935ff980d872a645f3ebbac0
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2022-02-06 08:56:33 -0700

    Change to classes ILO functions to avoid excessive global variables

--- main.py
+++ main.py
@@ -1,56 +1,107 @@
-# A GUI app to watermark an image
+# A watermarking GUI app utilizing classes
 
-from tkinter import Tk, Button, filedialog, Label
-from PIL import ImageTk, Image
+from tkinter import Entry, Tk, Button, filedialog, Label, StringVar
+from PIL import ImageTk, Image, ImageDraw, ImageFont
+import copy
 
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 500
-panel = None
-WATERMARK = None
+WM_FONT_SIZE = 80
 
 
-def open_image():
-    global panel
-    # Check for existing image in window and remove
-    if panel != None:
-        panel.destroy()
-    # Get filename and open image
-    filename = openfilename()
-    img = Image.open(filename)
-    img_width, img_height = img.size  # Find size of image for scaling
-    img_scale = (WINDOW_WIDTH - 50) / img_width  # Scale image to fit width
-    # Check height of image vs window height and reset scale if needed
-    if img_height * img_scale > WINDOW_HEIGHT - 30:
-        img_scale = (WINDOW_HEIGHT - 30) / img_height
-    preview_img = img.resize((int(img_width*img_scale),
-                              int(img_height*img_scale)),
-                             Image.ANTIALIAS)
-    preview_img = ImageTk.PhotoImage(preview_img)
-    panel = Label(image=preview_img)
-    panel.image = preview_img
-    panel.place(relwidth=1.0)
+class Watermarker(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.watermark = StringVar()
+        self.title('Simple Watermarking App')
+        self.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
+        # Open Image
+        open_button = Button(text="Open Image", command=self.open_image)
+        open_button.place(width=120, relx=0.5, x=-260, rely=1, y=-25)
+        # Watermark entry
+        wm_entry = Entry(0, textvariable=self.watermark)
+        wm_entry.insert(0, 'Watermark Text')
+        wm_entry.place(width=120, relx=0.5, x=-125, rely=1, y=-22)
+        # Preview button
+        preview_button = Button(text='Preview Watermark',
+                                command=self.make_watermark)
+        preview_button.place(width=120, relx=0.5, x=5, rely=1, y=-25)
+        # Save button
+        save_button = Button(text='Save Image', command=self.save_image)
+        save_button.place(width=120, relx=0.5, x=140, rely=1, y=-25)
+
+    def open_image(self):
+        try:
+            self.img_panel.destroy()
+        except AttributeError:
+            pass
+        filename = self.openfilename()
+        self.photo = Photo(Image.open(filename))
+        preview_image = ImageTk.PhotoImage(self.photo.img_small)
+        self.img_panel = Label(image=preview_image)
+        self.img_panel.image = preview_image
+        self.img_panel.place(relwidth=1.0)
+
+    def openfilename(self):
+        # Open file dialog box to select image
+        filename = filedialog.askopenfilename(initialdir='%USERPROFILE%',
+                                              title='Select an Image.',
+                                              filetypes=[('Image files',
+                                                          '*.jpg *.jpeg *.png'),
+                                                         ('All files', '*.*')])
+        return filename
+
+    def make_watermark(self):
+        self.img_panel.destroy()
+        watermark = self.watermark.get()
+        # Create working copy of image for preview
+        preview_wm_img = copy.copy(self.photo.img_small).convert('RGBA')
+        txt = Image.new('RGBA', preview_wm_img.size, (255, 255, 255, 0))
+        wm_font = ImageFont.truetype('./open-sans.extrabold.ttf', WM_FONT_SIZE)
+        d = ImageDraw.Draw(txt)
+        # Find preview image center for text
+        img_width, img_height = preview_wm_img.size
+        d.text((img_width/2, img_height/2), watermark,
+               font=wm_font, anchor='mm', fill=(0, 0, 0, 50))
+        combined_img = Image.alpha_composite(preview_wm_img, txt)
+        preview_image = ImageTk.PhotoImage(combined_img)
+        self.img_panel = Label(image=preview_image)
+        self.img_panel.image = preview_image
+        self.img_panel.place(relwidth=1.0)
+        self.watermark1 = watermark
+
+    def save_image(self):
+        final_img = self.photo.img.convert('RGBA')
+        txt = Image.new('RGBA', final_img.size, (255, 255, 255, 0))
+        wm_font = ImageFont.truetype('./open-sans.extrabold.ttf',
+                                     int(WM_FONT_SIZE/self.photo.img_scale))
+        d = ImageDraw.Draw(txt)
+        img_width, img_height = self.photo.img.size
+        d.text((img_width/2, img_height/2), self.watermark1,
+               font=wm_font, anchor='mm', fill=(0, 0, 0, 50))
+        combined_img = Image.alpha_composite(final_img, txt)
+        combined_img.show()
 
 
-def openfilename():
-    # Open file dialog box to select image
-    filename = filedialog.askopenfilename(initialdir='%USERPROFILE%',
-                                          title='Select an Image.',
-                                          filetypes=[('Image files',
-                                                      '*.jpg *.jpeg *.png'),
-                                                     ('All files', '*.*')])
-    return filename
+class Photo:
+    def __init__(self, img):
+        self.img = img
+        self.width, self.height = img.size
+        self.img_small = self.resize()
+
+    def resize(self):
+        img_scale = self.find_scale()
+        new_width = self.width * img_scale
+        new_height = self.height * img_scale
+        return self.img.resize((int(new_width), int(new_height)),
+                               Image.ANTIALIAS)
+
+    def find_scale(self):
+        self.img_scale = (WINDOW_WIDTH - 50) / self.width
+        if self.img_scale * self.height > WINDOW_HEIGHT - 30:
+            self.img_scale = (WINDOW_HEIGHT - 30) / self.height
+        return self.img_scale
 
 
-def main():
-    window = Tk()
-    window.title("Simple Watermarking App")
-    window.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
-    # window.resizable(width=True, height=True)
-    open_button = Button(text="Open Image", command=open_image)
-    # Place button at bottom
-    open_button.place(width=100, relx=0.5, x=-50, rely=1, y=-25)
-    window.mainloop()  # Keep the window open
-
-
-if __name__ == '__main__':
-    main()
+app = Watermarker()
+app.mainloop()
