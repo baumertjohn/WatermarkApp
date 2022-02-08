115b915677616a29c903e5e2c23bdcf8be3c37c0
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2022-02-07 20:31:18 -0700

    Added save method and popup dialog with filename.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # A watermarking GUI app utilizing classes
 
-from tkinter import Entry, Tk, Button, filedialog, Label, StringVar
+from tkinter import Entry, Tk, Button, filedialog, Label, StringVar, messagebox
 from PIL import ImageTk, Image, ImageDraw, ImageFont
 import copy
 
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 500
-WM_FONT_SIZE = 80
+WM_FONT_SIZE = 30
 
 
 class Watermarker(Tk):
@@ -35,8 +35,8 @@ class Watermarker(Tk):
             self.img_panel.destroy()
         except AttributeError:
             pass
-        filename = self.openfilename()
-        self.photo = Photo(Image.open(filename))
+        self.filename = self.openfilename()
+        self.photo = Photo(Image.open(self.filename))
         preview_image = ImageTk.PhotoImage(self.photo.img_small)
         self.img_panel = Label(image=preview_image)
         self.img_panel.image = preview_image
@@ -61,8 +61,8 @@ class Watermarker(Tk):
         d = ImageDraw.Draw(txt)
         # Find preview image center for text
         img_width, img_height = preview_wm_img.size
-        d.text((img_width/2, img_height/2), watermark,
-               font=wm_font, anchor='mm', fill=(0, 0, 0, 50))
+        d.text((img_width-10, img_height-10), watermark,
+               font=wm_font, anchor='rs', fill=(0, 0, 0, 50))
         combined_img = Image.alpha_composite(preview_wm_img, txt)
         preview_image = ImageTk.PhotoImage(combined_img)
         self.img_panel = Label(image=preview_image)
@@ -77,10 +77,30 @@ class Watermarker(Tk):
                                      int(WM_FONT_SIZE/self.photo.img_scale))
         d = ImageDraw.Draw(txt)
         img_width, img_height = self.photo.img.size
-        d.text((img_width/2, img_height/2), self.watermark1,
-               font=wm_font, anchor='mm', fill=(0, 0, 0, 50))
+        d.text((img_width-int(10/self.photo.img_scale),
+                img_height-int(10/self.photo.img_scale)), self.watermark1,
+               font=wm_font, anchor='rs', fill=(0, 0, 0, 50))
         combined_img = Image.alpha_composite(final_img, txt)
-        combined_img.show()
+        # combined_img.show()
+        # Find the file extension to separate and add "_watermark" to name
+        end_count = 1
+        for char in reversed(self.filename):
+            if char == '.':
+                break
+            end_count += 1
+        filename = self.filename[:-end_count]
+        extension = self.filename[-end_count:]
+        new_filename = f"{filename}_watermark{extension}"
+        # Save file after converting back to RGB (needed for jpeg files)
+        combined_img.convert('RGB').save(new_filename)
+        # Find first '/' to show user new filename in pop window.
+        end_count = 0
+        for char in reversed(new_filename):
+            if char == '/':
+                break
+            end_count += 1
+        messagebox.showinfo("Watermark App",
+                            f"Image saved as\n{new_filename[-end_count:]}")
 
 
 class Photo:
